b5cd699f374e185eb4ae1133aaf64ba0fa92dce0
samlm <9084029+samlm0@users.noreply.github.com>
2023-01-02 03:04:21 +0800

    refactor: command framework

---
+++ backend/app/clients/Websocket/Enums/WebsocketCommandEnum.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Client\Websocket\Enums;
+
+use Client\Websocket\Commands\{
+    Ping,
+    TraceRoute,
+    IPerf3
+};
+
+/**
+ * The websocket command map
+ */
+enum WebsocketCommandEnum: int
+{
+    /**
+     * function code
+     *
+     * base logic code
+     */
+    case End = 0;
+    case Config = 1000;
+
+    /**
+     * commands code
+     *
+     * add your feature to here
+     *
+     * don't forget add class to getClass() method
+     */
+    case Ping = 1;
+        // case TraceRoute = 2;
+    case IPerf3 = 4;
+
+
+    public function getClass(): ?string
+    {
+        return match ($this) {
+            self::Ping => Ping::class,
+            // self::TraceRoute => TraceRoute::class,
+            self::IPerf3 => IPerf3::class,
+            default => null
+        };
+    }
+
+    public function isEnable(): bool
+    {
+        return match ($this) {
+            self::Ping => env('UTILITIES_PING', true),
+            // self::TraceRoute => env('UTILITIES_TRACEROUTE', true),
+            self::IPerf3 => env('UTILITIES_IPERF3', true),
+            default => false
+        };
+    }
+}
--- ui/src/components/Utilities.vue
+++ ui/src/components/Utilities.vue
@@ -3,34 +3,17 @@
     <n-card>
       <template #header> 网络工具 </template>
       <n-space>
-        <n-button
-          v-show="componentConfig.utilities_ping"
-          @click="activate('ping')"
-          >Ping</n-button
-        >
-        <n-button
-          v-show="componentConfig.utilities_traceroute"
-          @click="activate('traceroute')"
-          >Traceroute</n-button
-        >
-        <n-button
-          v-show="componentConfig.utilities_iperf3"
-          @click="activate('iperf3')"
-          >iPerf3</n-button
-        >
+        <n-button v-show="componentConfig.utilities_ping" @click="activate('ping')">Ping</n-button>
+        <!-- <n-button v-show="componentConfig.utilities_traceroute" @click="activate('traceroute')">Traceroute</n-button> -->
+        <n-button v-show="componentConfig.utilities_iperf3" @click="activate('iperf3')">iPerf3</n-button>
       </n-space>
     </n-card>
-    <n-drawer
-      v-model:show="componentSwitch.ping"
-      :native-scrollbar="true"
-      :width="drawWidth"
-      placement="right"
-    >
+    <n-drawer v-model:show="componentSwitch.ping" :native-scrollbar="true" :width="drawWidth" placement="right">
       <n-drawer-content title="Ping" :closable="true">
         <ping v-model:ws="ws" v-model:wsMessage="wsMessage" />
       </n-drawer-content>
     </n-drawer>
-    <n-drawer
+    <!-- <n-drawer
       v-model:show="componentSwitch.traceroute"
       :native-scrollbar="true"
       :width="drawWidth"
@@ -39,19 +22,10 @@
       <n-drawer-content title="Traceroute" :closable="true">
         <traceroute v-model:ws="ws" v-model:wsMessage="wsMessage" />
       </n-drawer-content>
-    </n-drawer>
-    <n-drawer
-      v-model:show="componentSwitch.iperf3"
-      :native-scrollbar="true"
-      :width="drawWidth"
-      placement="right"
-    >
+    </n-drawer> -->
+    <n-drawer v-model:show="componentSwitch.iperf3" :native-scrollbar="true" :width="drawWidth" placement="right">
       <n-drawer-content title="iPerf3" :closable="true">
-        <iperf3
-          v-model:ws="ws"
-          v-model:wsMessage="wsMessage"
-          v-model:componentConfig="componentConfig"
-        />
+        <iperf3 v-model:ws="ws" v-model:wsMessage="wsMessage" v-model:componentConfig="componentConfig" />
       </n-drawer-content>
     </n-drawer>
   </div>
@@ -62,9 +36,9 @@ import { defineComponent, defineAsyncComponent } from "vue";
 export default defineComponent({
   components: {
     ping: defineAsyncComponent(() => import("./Utilities/Ping.vue")),
-    traceroute: defineAsyncComponent(() =>
-      import("./Utilities/Traceroute.vue")
-    ),
+    // traceroute: defineAsyncComponent(() =>
+    //   import("./Utilities/Traceroute.vue")
+    // ),
     iperf3: defineAsyncComponent(() => import("./Utilities/iPerf3.vue")),
   },
   props: {
