8f2bafaf75fd64c61b4f949f7dd6546c47e75f84
samlm <9084029+samlm0@users.noreply.github.com>
2023-01-08 16:43:32 +0800

    remove: drop cpulimit package

--- backend/app/components/98-fake_shell.php
+++ backend/app/components/98-fake_shell.php
@@ -1,8 +1,8 @@
 <?php
 if (env('UTILITIES_FAKESHELL', true) !== true) return;
 
-
+$groupInfo = posix_getgrgid("app");
 $process = new Swoole\Process(function (Swoole\Process $worker) {
-    $worker->exec('/usr/bin/ttyd', ['-b', '/shell', '-i', '/run/ttyd.sock', '-a', '/app/utilities/start_fakeroot.sh']);
+    $worker->exec('/usr/bin/ttyd', ['-g', $groupInfo['gid'], '-b', '/shell', '-i', '/run/ttyd.sock', '-a', '/app/utilities/start_fakeroot.sh']);
 });
 $process->start();
