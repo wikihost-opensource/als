23a38e8cab3a8489d318ec75cf9ab50759314044
samlm <9084029+samlm0@users.noreply.github.com>
2023-01-08 15:57:33 +0800

    fix: missing child process pid recycle

--- backend/app/app.php
+++ backend/app/app.php
@@ -5,6 +5,13 @@ applog('Application is starting...');
 
 Swoole\Runtime::enableCoroutine(SWOOLE_HOOK_ALL);
 
+Swoole\Process::signal(SIGCHLD, function ($sig) {
+    //必须为false，非阻塞模式
+    while ($ret = Swoole\Process::wait(false)) {
+        applog("Child process exited - PID: {$ret['pid']}");
+    }
+});
+
 foreach (glob(__DIR__ . '/components/*.php') as $component) {
     require_once $component;
 }
