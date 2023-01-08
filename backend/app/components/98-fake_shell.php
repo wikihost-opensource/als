f50b7cc60b487f589ac8d29bd0f749d5f3d5e7a6
samlm <9084029+samlm0@users.noreply.github.com>
2023-01-08 16:58:26 +0800

    fix: fake shell ttyd run permission

--- backend/app/components/98-fake_shell.php
+++ backend/app/components/98-fake_shell.php
@@ -1,8 +1,8 @@
 <?php
 if (env('UTILITIES_FAKESHELL', true) !== true) return;
 
-$groupInfo = posix_getgrgid("app");
-$process = new Swoole\Process(function (Swoole\Process $worker) {
+$groupInfo = posix_getgrnam("app");
+$process = new Swoole\Process(function (Swoole\Process $worker) use ($groupInfo) {
     $worker->exec('/usr/bin/ttyd', ['-g', $groupInfo['gid'], '-b', '/shell', '-i', '/run/ttyd.sock', '-a', '/app/utilities/start_fakeroot.sh']);
 });
 $process->start();
