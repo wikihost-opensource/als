e81b6911a7edcbab21f0234237d2e4bd7a741811
samlm0 <9084029+samlm0@users.noreply.github.com>
2023-03-30 16:33:05 +0800

    fix: #38

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -8,6 +8,7 @@ import Components from "unplugin-vue-components/vite";
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
